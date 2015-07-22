79e437fde80a85ed9361718ad4481659ad0b8fd4
Dmitry Borzov <tihoutrom@gmail.com>
2015-07-22 15:49:33 -0400

    Rename CLIProgressTracker to CLIProgressTracker in tests

--- datacats/tests/test_cli_progress_bar.py
+++ datacats/tests/test_cli_progress_bar.py
@@ -1,12 +1,11 @@
 import unittest
 
-from datacats.cli.util import CliProgressTracker
+from datacats.cli.util import CLIProgressTracker
 import random
 
-
-class TestCliProgressTracker(unittest.TestCase):
+class TestCLIProgressTracker(unittest.TestCase):
     def test_progress_tracker(self):
-        with CliProgressTracker(
+        with CLIProgressTracker(
             task_title="Title for test progress bar",
             total=13) as pt:
             for i in range(14):
@@ -17,6 +16,7 @@ class TestCliProgressTracker(unittest.TestCase):
                         'total': 13,
                         'status': 'wo' * random.randint(3, 14)}
                     )
+
             pt.update_state(
                 state='BANANA',
                 meta={
