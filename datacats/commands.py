b40e24fae7de3026ef6794863d5d444f92d9e521
Dmitry Borzov <tihoutrom@gmail.com>
2015-07-22 13:09:42 -0400

    Stub for commands.py where all the package interface-level functions are to be defined

---
+++ datacats/commands.py
@@ -0,0 +1,14 @@
+# Copyright 2014-2015 Boxkite Inc.
+
+# This file is part of the DataCats package and is released under
+# the terms of the GNU Affero General Public License version 3.0.
+# See LICENSE.txt or http://www.fsf.org/licensing/licenses/agpl-3.0.html
+
+"""
+The commands module defines the datacats package's interface, that is
+all the commands the package implements as functions.
+
+The rest of the datacats package is
+ - implementation of these commands
+ - a shell wrapper of these commands for datacats-cli tool (see cli module)
+"""
